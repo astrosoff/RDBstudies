ebf2d61457b004b2a4b7e7cb00398e2dd03eafe9
astrosoff <sofian.bouhlou@gmail.com>
2023-08-09 06:09:37 +0000

    Save progress: 1270.1

--- student_info.sh
+++ student_info.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 #Info about my computer science students from students database
 echo -e "\n~~ My Computer Science Students ~~\n"
+PSQL="psql -X --username=freecodecamp --dbname=students --no-align --tuples-only -c"
